d4334a5e6c48ecc18585d255f024b5630ab64ba0
Daniel Maelzer <daniel.maelzer@mercedes-benz.com>
2024-06-02 21:49:20 +0200

    further work on about

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -16,42 +16,42 @@ class About extends React.Component {
             <img src={process.env.PUBLIC_URL + "/images/aboutme.jpg"} className="intro-image" width="50%"/>
           </div>
           <div className="intro-text">
-            <h1>Hi, ich bin Daniel!</h1>
+            <h1 className="introHeading"><span style={{color: '#FF4093'}}>Hi</span>, ich bin <span style={{color: '#FF7F1D'}}>Daniel</span>!</h1>
             <p>Ich bin Informatiker/Webentwickler aus Berlin.
                 Ich entwickle Webseiten und helfe Ihnen digitale Lösungen für Ihr Unternehmen zu gestalten.</p>             
             <div className="download-buttons">
               <div className="download-button-1">
-                <a href="/uploads/cv-ger.pdf"><button className="about"><img src="images/icons/download.png" className="download-image" width="12%"/>Lebenslauf</button></a>
+                <a href="/uploads/cv-ger.pdf"><button className="about portfolio"><img src="images/icons/download.png" className="download-image" width="20px"/>Lebenslauf</button></a>
               </div>
               <div className="download-button-2">
-                <a href="https://www.linkedin.com/in/daniel-maelzer-3934541a7/details/certifications/"><button className="portfolio"><img src="images/icons/download.png" className="download-image" width="12%"/>Zertifikate</button></a>
+                <a href="https://www.linkedin.com/in/daniel-maelzer-3934541a7/details/certifications/"><button className="portfolio"><img src="images/icons/download.png" className="download-image" width="20px"/>Zertifikate</button></a>
               </div>
             </div>
           </div>
         </div>
 
-      <h1 className="serviceHeading">Services</h1>
+      <h1 className="serviceHeading"><span style={{color: '#FF7F1D'}}>Services</span></h1>
 
       <div className="services-section">
         <div className="wordpress">
           <img className="serviceIcon" src="images/services/wordpress-logo.png"/>
           <h2 className="serviceSubHeading">Wordpress</h2>
-          <p>Eine Webseite für das eigene Geschäft oder Webshop mit Warenwirtschaftssystem für den großen Verlag, Ich biete Ihnen eine sorglose und transparente Komplettlösung.</p>      
+          <p className="serviceText">Eine Webseite für das eigene Geschäft oder Webshop mit Warenwirtschaftssystem für den großen Verlag, Ich biete Ihnen eine sorglose und transparente Komplettlösung.</p>      
         </div>
         <div className="seo">
           <img className="serviceIcon" src="images/services/analysis.png"/>
           <h2 className="serviceSubHeading">SEO</h2>
-          <p>Ich baue mit Ihnen robuste SEO Strategien, damit Ihr Business noch erfolgreicher im Web wird. Durch den Einsatz von Analyse Tools können Sie den Fortschritt bequem von Zuhause verfolgen.</p>      
+          <p className="serviceText">Ich baue mit Ihnen robuste SEO Strategien, damit Ihr Business noch erfolgreicher im Web wird. Durch den Einsatz von Analyse Tools können Sie den Fortschritt bequem von Zuhause verfolgen.</p>      
         </div>
         <div className="maintenance">
           <img className="serviceIcon" src="images/services/tools.png"/>
           <h2 className="serviceSubHeading">Maintenance</h2>
-          <p>Ich kümmere mich gerne um die Weiterentwicklung, Umgestaltung, Wartung oder Neu-Installation Ihrer WordPress Webseite. Ich stehe Ihnen auch ohne Umwege beratend zur Verfügung.</p>      
+          <p className="serviceText">Ich kümmere mich gerne um die Weiterentwicklung, Umgestaltung, Wartung oder Neu-Installation Ihrer WordPress Webseite. Ich stehe Ihnen auch ohne Umwege beratend zur Verfügung.</p>      
         </div>
         <div className="hosting">
           <img className="serviceIcon" src="images/services/database.png"/>
           <h2 className="serviceSubHeading">Hosting</h2>
-          <p>Ich optimiere die Ladezeiten ihrer Webseite und kümmere mich dabei um das optimale Hostingangebot, regelmäßige Sicherungen oder den eigenen E-Mailserver.</p>      
+          <p className="serviceText">Ich optimiere die Ladezeiten ihrer Webseite und kümmere mich dabei um das optimale Hostingangebot, regelmäßige Sicherungen oder den eigenen E-Mailserver.</p>      
         </div>
       </div>
 
