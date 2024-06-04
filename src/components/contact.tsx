2eac470d622a7a2a6783362ed6ac82ffba806397
Daniel Maelzer <daniel.maelzer@mercedes-benz.com>
2024-06-04 21:57:55 +0200

    easy simple contact content and basic styling

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -3,12 +3,14 @@ import "../css/contact.css";
 
 const Contact = () => {
   const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
+  const [phone, setPhone] = useState('');
+  const [mail, setMail] = useState('');
+  const [firma, setFirma] = useState('');
+  const [anfrage, setAnfrage] = useState('');
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(`Name: ${name}, Email: ${email}, Message: ${message}`);
+    console.log(`Name: ${name}, Phone: ${phone}, Mail: ${mail}, Firma: ${firma}, Problem: ${anfrage}`);
   };
 
   return (
@@ -17,22 +19,19 @@ const Contact = () => {
         <a className="mobiletext" href="/">zurück</a>
         <a className="mobiletext" href="/about">about</a>
       </nav>
+      <h1 className='contact-heading'>Du hast eine Frage?</h1>
       <form onSubmit={handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={name} onChange={e => setName(e.target.value)} required />
-        </label>
-        <label>
-          Email:
-          <input type="email" value={email} onChange={e => setEmail(e.target.value)} required />
-        </label>
-        <label>
-          Message:
-          <textarea value={message} onChange={e => setMessage(e.target.value)} required />
-        </label>
-        <button type="submit">Submit</button>
+        <div className='contact'>
+          <input className='' type="text" id="name" name="name" value={name} onChange={e => setName(e.target.value)} placeholder='Name*' required/>
+          <input className='' type="text" id="phone" name="phone" value={phone} onChange={e => setPhone(e.target.value)} placeholder='Telefonnummer'/>
+          <input className='' type="text" id="firma" name="firma" value={firma} onChange={e => setFirma(e.target.value)} placeholder='Firma'/>
+          <input className='' type="text" id="mail" name="mail" value={mail} onChange={e => setMail(e.target.value)} placeholder='E-Mail Adresse*' required/>
+          <textarea className='' id="anfrage" name="anfrage" value={anfrage} onChange={e => setAnfrage(e.target.value)} placeholder='Dein Anliegen*' required/>
+          <input className='' type="submit" value="Absenden"/>
+        </div>
       </form>
     </div>
+
   );
 };
 
